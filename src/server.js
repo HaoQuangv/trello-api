394ecadffd278447b864b771427b01754279b758
Quang Hao <quanghao@Quangs-MacBook-Air.local>
2024-06-16 22:17:17 +0700

    connect mongodb and set up env variables

--- src/server.js
+++ src/server.js
@@ -1,32 +1,56 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hi ${ env.AUTHOR }, Back-end Server is running successfully at Host: ${ env.APP_HOST } and Port: ${ env.APP_PORT }`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
-})
+  // Thuc hien cac tac vu clean up truoc khi dung server
+  //Doc them o day: https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+  exitHook(() => {
+    console.log('4. Server is shutting down...')
+    CLOSE_DB()
+    console.log('5. Disconnected from MongoDB Cloud Atlas!')
+  })
+}
+
+// Chi khi ket noi toi Database thanh cong thi moi Start Server Back-end len
+// Immidiately Invoked Function Expression (IIFE)/ Anonymous Async Function(IIFE)
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Clous Atlas!')
+
+    // Khoi dong Server Back-end sau khi Connect Database thanh cong
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// Chi khi ket noi toi Database thanh cong thi moi Start Server Back-end len
+// console.log('1. Connecting to MongoDB Atlas...')
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDB Clous Atlas!'))
+//   .then(() => START_SERVER())
+//   .catch( error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
