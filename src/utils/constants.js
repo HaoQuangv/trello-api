172756a7be2172bb6bd29c6d0023a7911dcd6431
Quang Hao <quanghao@Quangs-MacBook-Air.local>
2024-06-24 15:33:23 +0700

    config_cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,9 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+// Nhung domain duoc phep truy cap den tai nguyen cua server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  // ...vv vi du sau nay se deloy len domain chinh thuc...
+]
