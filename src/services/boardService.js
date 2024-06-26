47b469849682ad45c7d10ee629eb1a968aaef60c
Quang Hao <quanghao@Quangs-MacBook-Air.local>
2024-06-26 09:59:50 +0700

    mongodb_aggregate

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -8,6 +8,7 @@ import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 
 const createNew = async (reqBody) => {
   try {
@@ -38,7 +39,23 @@ const getDetails = async (boardId) => {
     if (!board) {
       throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
     }
-    return board
+
+    // B1: Deep Clone board ra môt cái moi de xu ly, không änh huöng tói board ban däu, tuy muc dich vè sau mà có cân clone deep hay không. (video 63 se giai thich)
+    // https://www.javascripttutorial.net/javascript-primitive-vs-reference-values/
+    const resBoard = cloneDeep(board)
+
+    //B2: Dua card ve dung column cua no
+    resBoard.columns.forEach(column => {
+      // Cach dung equals nay la boi vi chúng ta hieu ObjectId trong MongoDB có support method equals
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+      // Cach khac don gian là convert ObjectId vè string bäng ham tostring() cua JavaScript
+      //column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+    })
+
+    //B3: Xoa mang cards khoi board ban dau
+    delete resBoard.cards
+
+    return resBoard
   } catch (error) {throw error}
 }
 
